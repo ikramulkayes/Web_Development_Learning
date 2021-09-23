dfaf2c1feedfdaa50ff0a93ba9d91748602e3dae
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-24 03:31:43 +0600

    before the end js for dom

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,3 +1,7 @@
 document.getElementsByTagName("li")[2].style.color = "red"
 document.querySelector(".list a").style.color = "green"
 document.querySelectorAll(".item")[1].style.color = "yellow"
+//document.querySelector("body").style.backgroundColor = "grey"
+//document.querySelector(".btn").style.backgroundColor = "blue"
+document.querySelector("button").classList.toggle("invisible");
+document.querySelector("h1").classList.add("huge")

814fa58f80af4b65fd8c367a86bc510fd1170ace
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-24 03:47:37 +0600

    done with dom js file

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -5,3 +5,5 @@ document.querySelectorAll(".item")[1].style.color = "yellow"
 //document.querySelector(".btn").style.backgroundColor = "blue"
 document.querySelector("button").classList.toggle("invisible");
 document.querySelector("h1").classList.add("huge")
+document.querySelector("h1").innerHTML = "<em>You have been hacked</em>"
+document.querySelector("a").setAttribute("href","https://github.com/ikramulkayes?tab=repositories")
